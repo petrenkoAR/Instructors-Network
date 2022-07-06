c15fa78e1df809deb765c1630fe74ba371ed1fc1
ARPetrenko <ms.suslikmur@mail.ru>
2022-07-06 16:39:14 +0300

    V 0.0.2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
+let questions = {
+  
+}
+
+function testClosed(n){
+  console.log('Кто-то вышел');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   container = document.getElementsByClassName('main')[0]
   frame = document.createElement('div')
   frame.classList.add('frame')
   container.appendChild(frame)
   header = document.createElement('p')
-  header.textContent = "Тест";
+  header.textContent = "Тест №1";
   frame.appendChild(header)
   window.onblur = () => console.log( 'Кто-то вышел' );
 })
 
-function testClosed(n){
-  console.log('Кто-то вышел');
+function createTest() {
+
 }
